76d35e4b5b5f4e9cb9675f4f24e77939517de049
dhertle <david.hertle@gmx.net>
2017-02-21 09:48:21 +0100

    Files added

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorTaxationScandinavia"
+  
+  # Name of problem sets in the package
+  ps = c("Taxation_Scandinavia")
+  
+  list(package=package, ps = ps)
+}
